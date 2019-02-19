993d0f75c94d7622d04408776400b5de0f4e079f
LeeKLTW <LeeKLTW@gmail.com>
2019-02-19 12:20:51 +0800

    new client

--- client.py
+++ client.py
@@ -6,6 +6,6 @@ PORT = 50007
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     s.sendall(b'hello')
-    data = s.recv()
+    data = s.recv(1024)
 print('Received',repr(data))
 
