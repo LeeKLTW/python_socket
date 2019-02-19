d14fa0d4683f9b8d5106a8dc252db27c69a6ecec
LeeKLTW <LeeKLTW@gmail.com>
2019-02-19 12:54:44 +0800

    Print server listening

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+print('Server starts listening.')
 
 while True:
     conn, addr = s.accept()
-    print('Connected by', addr)
-    # print(str(addr[0])+':'+str(addr[1]))
+    # print('Connected by', addr)
+    print(str(addr[0])+':'+str(addr[1]))
 
     while True:
         data = conn.recv(1024)
