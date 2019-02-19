4f24a7bb7d64d03375180964e6814aeaf0dcc69b
LeeKLTW <LeeKLTW@gmail.com>
2019-02-19 12:48:48 +0800

    interactive session

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 # encoding: utf-8
 import socket
+
 s = socket.socket()
 HOST = ''
 PORT = 50007
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
-    s.sendall(b'hello')
-    data = s.recv(1024)
-print('Received',repr(data))
-
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        ping = str(input("input or exit>>"))
+        if ping == 'exit':
+            print('exit')
+            break
+        s.sendall(ping.encode())
+        data = s.recv(1024)
+        print('Received', repr(data))
--- server.py
+++ server.py
@@ -3,14 +3,18 @@ import socket
 
 HOST = ''
 PORT = 50007
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-   s.bind((HOST,PORT))
-   s.listen(1)
-   conn, addr = s.accept()
 
-   with conn:
-       print('Connected by', addr)
-       while True:
-           data = conn.recv(1024)
-           if not data: break
-           conn.sendall(data)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+while True:
+    conn, addr = s.accept()
+    print('Connected by', addr)
+
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        else:
+            conn.sendall(b'PONG' + data)
