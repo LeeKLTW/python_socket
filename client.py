cf1fdfaffe24570757cde1007340643d5080692b
LeeKLTW <LeeKLTW@gmail.com>
2019-02-19 12:20:14 +0800

    new client

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 import socket
 s = socket.socket()
-host = socket.gethostname()
-port = 1234
+HOST = ''
+PORT = 50007
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    s.sendall(b'hello')
+    data = s.recv()
+print('Received',repr(data))
 
-s.connect((host,port))
-print(s.recv(1024))
-s.close()
